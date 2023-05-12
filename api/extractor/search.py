26ab08fab8b11552616be80b77f2ea289b351f55
Dheison Gomes <dheisomgomes0@gmail.com>
2023-05-12 09:32:17 -0300

    Search working

--- api/extractor/search.py
+++ api/extractor/search.py
@@ -1,6 +1,6 @@
 from .. import FRONT_PAGE
 from ..utils import get
-from bs4 import BeautifulSoup, NavigableString
+from bs4 import BeautifulSoup, Tag
 from dataclasses import dataclass
 from enum import Enum
 
@@ -64,15 +64,47 @@ async def getSearchResults(query: str, language: str = "",
         params={
             'q': query,
             'lang': language,
-            'ext': file_type,
-            'sort': order_by
+            'ext': file_type.value,
+            'sort': order_by.value
         }
     )
     html = BeautifulSoup(response.text, 'lxml')
     raw_results = html.findAll('div', class_='h-[125]')
     results = list(map(parseResult, raw_results))
-    return results
+    return [i for i in results if i != None]
 
 
-def parseResult(raw_content: NavigableString) -> Result:
-    pass
+def parseResult(raw_content: Tag) -> Result | None:
+    try:
+        title = raw_content.find('h3').getText(strip=True)
+    except:
+        return None
+    authors = raw_content.find(
+        'div',
+        class_='truncate italic'
+    ).getText(strip=True).split(', ')
+    publish_info = raw_content.find(
+        'div', class_='truncate text-sm'
+    ).text.split(', ')
+    publisher = publish_info[0]
+    publish_date = ', '.join(publish_info[1:])
+    thumbnail_url = raw_content.find('img').get('src')
+    url = raw_content.find('a').get('href')
+    file_info = extractFileInfo(
+        raw_content.find('div', class_='truncate text-xs text-gray-500').text
+    )
+    return Result(
+        title, authors, publisher, publish_date,
+        thumbnail_url, url, file_info
+    )
+
+
+def extractFileInfo(raw: str) -> FileInfo:
+    # raw i.e: English [en], pdf, 7.5MB, "Python_Web_Scraping_-_Second_Edition.pdf"
+    info_list = raw.split(', ')
+    return FileInfo(
+        name=info_list[-1].strip().replace('"', ''),
+        language=info_list[0],
+        extension=info_list[1],
+        size=info_list[2]
+    )
