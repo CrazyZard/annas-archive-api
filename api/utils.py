8d109ed2964564ce7d6b22c2aa9b1a1685763d0d
Dheison Gomes <dheisomgomes0@gmail.com>
2023-06-15 21:59:51 -0300

    Fix caching implementation

--- api/utils.py
+++ api/utils.py
@@ -3,6 +3,9 @@ from aiohttp import ClientSession
 from dataclasses import dataclass
 from sanic.request import Request
 from sanic.response import HTTPResponse
+from asyncio import create_task, sleep
+
+CACHE_TIME = 3600
 
 
 @dataclass
@@ -28,13 +31,26 @@ async def get(url: str, params: dict = {}) -> Response:
 def cache(func):
     cache_storage: dict[str, Cache] = {}
 
+    async def clean_cache():
+        while len(cache_storage) > 0:
+            try:
+                print(func, len(cache_storage))
+                for url, cache in cache_storage.items():
+                    if time() >= cache.expires:
+                        cache_storage.pop(url)
+            except:
+                continue
+            await sleep(1)
+
     async def wrapper(request: Request, *args, **kwargs) -> HTTPResponse:
         cache_data = cache_storage.get(request.url)
         if not cache_data or cache_data.expires < time():
             response = await func(request, *args, **kwargs)
-            cache_storage[request.url] = Cache(response, time() + 3600)
+            cache_storage[request.url] = Cache(response, time() + CACHE_TIME)
             cache_data = cache_storage[request.url]
         expires_in = int(cache_data.expires - time())
         cache_data.response.headers["Cache-Control"] = f"max-age={expires_in}"
+        if len(cache_storage) == 1:
+            create_task(clean_cache())
         return cache_data.response
     return wrapper
